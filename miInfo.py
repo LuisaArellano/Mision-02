44d7fa6da31f23f17d176ba83d7c13520b1f27f6
LuisaArellano <42380922+LuisaArellano@users.noreply.github.com>
2018-08-21 22:37:38 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Luisa Fernanda Arellano Alvarado 
+# Descripcion: Datos del alumno
 # Escribe tu programa después de esta línea.
+nombre = Luisa Fernanda Arellano Alvarado 
+matricula = A01377974
+carrera = ITI
+procedencia = Prepa Tec Bicultural 
+descripción = Me gusta nadar y bailar
+
+print("Nombre:", nombre)
+print("Matricula:", matricula)
+print("Carrera:", carrera)
+print("Procedencia:", procedencia)
+print("Descripción:", descripción) 
