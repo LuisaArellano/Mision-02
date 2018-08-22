958979f05bf07b9a5d354d5cae287142f3757148
LuisaArellano <42380922+LuisaArellano@users.noreply.github.com>
2018-08-22 00:31:52 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,19 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:Luisa Fernanda Arellano Alvarado, A01377974
+# Descripcion: Programa que te ayuda a saber el porcentaje que hay de ciertos ojetos o cantidades 
 
-# Escribe tu programa después de esta línea.
+a = int(intput("Teclea cantidad de mujeres"))
+b = float(intput("Teclea cantidad de hombres))
+
+c = a + b                            
+TotalMujeres = (a*100)/c
+TotalHombres = (b*100)/c
+
+print("Mujeres inscritas:",a)
+print("Hombres inscritos:",b)
+print("Porcentaje de Mujeres:",TotalMujeres)
+print("Porcentaje de Hombres:",TotalHombres) 
+
+
+
+
+
